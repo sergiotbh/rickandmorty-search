bb0d3512833ebee9a7b40239774d196f8870ce11
Sergio <sergiotbh@Sergios-MacBook-Air.local>
2022-08-12 01:48:25 -0500

    create search bar with api request

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,5 @@
 export const APP_WHITE = '#f5f5f5';
-export const LIGHT_GREY = '#bdc3c7';
+export const LIGHT_GREY = '#dedede';
 export const NEUTRAL_GREY = '#9e9e9e';
 export const MEDIUM_GREY = '#3c3e44'
 export const DARK_GREY = '#202329';
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,71 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { APP_WHITE, DARK_GREY, LIGHT_GREY, NEUTRAL_GREY } from '../colors';
+import { Feather } from '@expo/vector-icons';
+import { filterCharacter } from '../api';
+
+const ScreenContainer = styled.View`
+  flex: 1;
+  background-color: ${APP_WHITE};
+  padding: 10px;
+`
+
+const SearchButton = styled.TouchableOpacity`
+  padding: 10px;
+`
+
+const SearchBarWrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  background-color: ${LIGHT_GREY};
+  color: ${DARK_GREY};
+  border-radius: 10px;
+`
+
+const SearchInput = styled.TextInput`
+  flex: 1;
+  padding: 10px;
+`
 
 const HomeScreen = () => {
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    console.log(results)
+  }, [results])
+
+  const handleSearch = (searchTerm) => {
+    filterCharacter(searchTerm)
+    .then(res => setResults(res.results))
+    .catch(e => console.log(e))
+  }
+
   return(
-    <>
-      <Text>HOME</Text>
-    </>
+    <ScreenContainer>
+      <SearchBar onSearch={handleSearch}/>
+      
+    </ScreenContainer>
+  )
+}
+
+const SearchBar = ({onSearch}) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return(
+    <SearchBarWrapper>
+      <SearchInput
+        value={searchTerm}
+        onChangeText={text => setSearchTerm(text)}
+        placeholder="Escribe el nombre del personaje..."
+        placeholderTextColor={NEUTRAL_GREY}
+      />
+      <SearchButton
+        onPress={() => onSearch(searchTerm)}
+      >
+        <Feather name='search' size={24} color={DARK_GREY} />
+      </SearchButton>
+    </SearchBarWrapper>
   )
 }
 
